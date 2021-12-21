1f462d091395939c2aa8d800c09503cd2fe90b58
Programm0 <1Pro2gra3mm45@web.de>
2021-12-21 14:55:24 +0100

    Testing resource management

---
+++ plugz-test/src/main/java/com/programm/projects/plugz/test/MyResource.java
@@ -0,0 +1,14 @@
+package com.programm.projects.plugz.test;
+
+import com.programm.projects.plugz.magic.api.Resource;
+import lombok.ToString;
+
+@Resource("log-default.properties")
+@Resource("log.properties")
+@ToString
+public class MyResource {
+
+    public String logLevel;
+    public String logFormat;
+
+}
--- plugz-test/src/main/java/com/programm/projects/plugz/test/TestService.java
+++ plugz-test/src/main/java/com/programm/projects/plugz/test/TestService.java
@@ -2,10 +2,7 @@ package com.programm.projects.plugz.test;
 
 import com.programm.projects.ioutils.log.api.out.ILogger;
 import com.programm.projects.ioutils.log.api.out.Logger;
-import com.programm.projects.plugz.magic.api.Get;
-import com.programm.projects.plugz.magic.api.ISchedules;
-import com.programm.projects.plugz.magic.api.Scheduled;
-import com.programm.projects.plugz.magic.api.Service;
+import com.programm.projects.plugz.magic.api.*;
 
 @Service
 @Logger
@@ -14,12 +11,20 @@ public class TestService {
     @Get
     private ILogger log;
 
+    @Get
+    private MyResource res;
+
     private int i;
+
+    @PostSetup
+    private void init(){
+        log.info("Initial: {}", res);
+    }
 
     @Scheduled(repeat = 1000)
     private void test(@Get ISchedules schedules){
         i++;
-        log.info("" + i);
+        log.info("{}", i);
 
         if(i >= 10){
             schedules.stopScheduler();
