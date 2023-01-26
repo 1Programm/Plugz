731bd483a2f7df1c660fff321672babcac45230a
Programm0 <1Pro2gra3mm45@web.de>
2023-01-26 12:58:28 +0100

    Cleanup test

--- plugz-test/src/main/java/com/programm/plugz/test/Application.java
+++ plugz-test/src/main/java/com/programm/plugz/test/Application.java
@@ -1,45 +1,24 @@
 package com.programm.plugz.test;
 
+import com.programm.ioutils.log.api.ILogger;
+import com.programm.plugz.api.Service;
 import com.programm.plugz.api.auto.Get;
+import com.programm.plugz.api.lifecycle.PostStartup;
 import com.programm.plugz.magic.MagicEnvironment;
-import com.programm.plugz.webserv.api.GetMapping;
-import com.programm.plugz.webserv.api.PostMapping;
-import com.programm.plugz.webserv.api.RequestBody;
-import com.programm.plugz.webserv.api.RestController;
 
-import java.util.List;
-
-@RestController("/person")
+@Service
 public class Application {
 
     public static void main(String[] args) throws Exception {
         MagicEnvironment.Start(args);
     }
 
-    @GetMapping("/all")
-    public List<Person> getAll(@Get PersonRepo personRepo){
-        return personRepo.findAll();
+    @Get private ILogger log;
+
+    @PostStartup
+    public void setup(@Get String abc){
+        log.info("> Repo: {}", "Test", abc);
     }
 
-    @GetMapping("/gen")
-    public String generateRandomPerson(@Get PersonRepo personRepo){
-        Person person = new Person();
-        person.name = "Test Person";
-        person.age = (int)(Math.random() * 99 + 1);
-
-        personRepo.save(person);
-
-        return "Generated a new Person";
-    }
-
-    @PostMapping("/save")
-    public void savePerson(@Get PersonRepo personRepo, @RequestBody Person person){
-        personRepo.save(person);
-    }
-
-    @PostMapping("/delete")
-    public void deletePerson(@Get PersonRepo personRepo, @RequestBody Person person){
-        personRepo.delete(person);
-    }
 
 }
